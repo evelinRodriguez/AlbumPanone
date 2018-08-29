ba462ae2b90a8be1418fb857a4de899eadcde59d
Estudiantes <Estudiantes@Labing70607>
2018-08-29 17:41:58 -0500

    juego 1.0

---
+++ src/albumpanone/AlbumPanone.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albumpanone;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class AlbumPanone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Jugador miJugador=new Jugador();
+         
+    }
+}
---
+++ src/albumpanone/Jugador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albumpanone;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Jugador {
+    String nombre;
+    int edad;
+    int codigo;
+    int programacion;
+    int lenguaje;
+    int trabajoenequipo;
+    int dinero;
+    
+    void calcularPotencial(){
+        
+    }
+    
+}
